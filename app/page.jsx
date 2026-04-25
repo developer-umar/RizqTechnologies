aa19bc0dc1ba8f8c8570d42c0b5a49a66228070c
Mohammad Umar <mohammadumar8010@gmail.com>
2026-04-25 10:20:59 +0530

    fixed about

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ import Portfolio from "./components/Portfolio";
 import Pricing from "./components/Pricing";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import AboutUsLight from "./components/AboutUse";
+import AboutUsPremium from "./components/AboutUse";
 
 // import ClientCursor from "./components/cursor/clientCursor";
 
@@ -29,7 +29,7 @@ export default function Home() {
         {/* <ClientCursor /> */}
         <Navbar />
         <HeroSection />
-        <AboutUsLight/>
+        <AboutUsPremium/>
         <Services />
         <Portfolio />   
         <Pricing />
