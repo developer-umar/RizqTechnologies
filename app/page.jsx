d3e2641ab19917737820506abe6ddebe2f0f3329
Mohammad Umar <mohammadumar8010@gmail.com>
2026-05-16 01:20:01 +0530

    updated pages.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -54,7 +54,7 @@ export default function Home() {
     <>
       <div id="home">
         <ScrollManager />
-        // <ClientCursor/>
+        {/* // <ClientCursor/> */}
         <Navbar />
         <HeroSection />
         <AboutUsFluid />
